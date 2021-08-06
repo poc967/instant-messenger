e13e551e6aa59d5dca7847323eff9a85ea664523
Patrick O'Connor <poc.967@gmail.com>
2021-08-05 22:18:56 -0400

    add support for images in convo cards and messager heading

--- client/src/components/Messager.js
+++ client/src/components/Messager.js
@@ -21,13 +21,50 @@ const NameBanner = styled.div`
   justify-content: center;
   align-items: flex-start;
   width: 100%;
-  height: 4rem;
+  height: auto;
   color: black;
 `;
 
 const Name = styled.span`
   font-size: 1rem;
   font-weight: 200;
+  margin-left: 1rem;
+`;
+
+const UserImage = styled.div`
+  background-image: url(${(props) => props.picture});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  width: 5rem;
+  height: 5rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  color: black;
+`;
+
+const GenericAvatar = styled.div`
+  background-color: rgb(22, 204, 152);
+  display: flex;
+  flex-direction: column;
+  width: 5rem;
+  height: 5rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  color: white;
+`;
+
+const ImageAlign = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
 `;
 
 const Messager = (props) => {
@@ -40,7 +77,20 @@ const Messager = (props) => {
       ) : (
         <>
           <NameBanner>
-            <Name>{`${props.conversation.title.firstName} ${props.conversation.title.lastName}`}</Name>
+            <ImageAlign>
+              {props.conversation.title.picture !== null ? (
+                <UserImage picture={props.conversation.title.picture} />
+              ) : (
+                <GenericAvatar>
+                  <span>{`${props.conversation.title.firstName
+                    .split("")[0]
+                    .toUpperCase()}${props.conversation.title.lastName
+                    .split("")[0]
+                    .toUpperCase()}`}</span>
+                </GenericAvatar>
+              )}
+              <Name>{`${props.conversation.title.firstName} ${props.conversation.title.lastName}`}</Name>
+            </ImageAlign>
           </NameBanner>
           <Messages
             activeConversationId={props.activeConversationId}
